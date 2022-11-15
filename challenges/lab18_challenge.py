5a522a5ecd6c21774ed45a89179259feda0045cf
albert <almez5202@gmail.com>
2022-11-15 21:57:50 +0000

    mixed lists

---
+++ challenges/lab18_challenge.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def makeGreeting():
+    name = input("Please enter your name: ")
+    day = input("Please enter what day of the week it is: ")
+    print(f"Hello, {name}! Happy {day}!")
+
+makeGreeting()
